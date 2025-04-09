5afe472a021259e978cfc1c87508ac62b71b762d
Stephanie Mortemousque <smortemo@1C1.42angouleme.fr>
2025-04-09 16:25:35 +0200

    added = and copy in Atarget ASpell

--- cpp_module01/ASpell.cpp
+++ cpp_module01/ASpell.cpp
@@ -6,7 +6,7 @@
 /*   By: smortemo <smortemo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:01:02 by smortemo          #+#    #+#             */
-/*   Updated: 2025/02/04 19:03:22 by smortemo         ###   ########.fr       */
+/*   Updated: 2025/04/09 16:12:41 by smortemo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,20 @@
     ASpell::ASpell(std::string name, std::string effect): _name(name), _effects(effect) {}
     
     ASpell::~ ASpell(void) {}
+
+
+    ASpell::ASpell(ASpell &other): _name(other._name), _effects(other._effects) {}
+    
+    ASpell& ASpell::operator=(ASpell &other)
+    {
+        if (this != &other)
+        {
+            this->_name = other._name;
+            this->_effects = other._effects;
+        }
+        return *this;
+    }
+
 
     const std::string  ASpell::getName(void) const 
     { return (_name);}
--- cpp_module01/ATarget.cpp
+++ cpp_module01/ATarget.cpp
@@ -6,7 +6,7 @@
 /*   By: smortemo <smortemo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:01:02 by smortemo          #+#    #+#             */
-/*   Updated: 2025/02/04 19:05:38 by smortemo         ###   ########.fr       */
+/*   Updated: 2025/04/09 16:13:40 by smortemo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@
     ATarget::ATarget(std::string type): _type(type) {}
     
     ATarget::~ ATarget(void) {}
+
+    ATarget::ATarget(ATarget &other): _type(other._type) {}
+    
+    ATarget& ATarget::operator=(ATarget &other)
+    {
+        if (this != &other)
+            this->_type = other._type;
+        return *this;
+    }
 
     const std::string  ATarget::getType(void) const 
     { return(_type);}
